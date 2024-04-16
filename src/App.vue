9eed21c1a051cbd99371fbefaebe57f6bc34705c
parsehex <1779937+parsehex@users.noreply.github.com>
2024-04-16 18:25:49 -0400

    change to vue, npm audit fix, prep for domain

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="App">
+		<header class="App-header">
+			<img :src="logo" class="App-logo" alt="logo" />
+			<p>Hello Vite + Vue!</p>
+			<p>
+				<button type="button" @click="incrementCount">
+					count is: {{ count }}
+				</button>
+			</p>
+			<p>Edit <code>App.vue</code> and save to test HMR updates.</p>
+			<p>
+				<a
+					class="App-link"
+					href="https://vuejs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn Vue
+				</a>
+				|
+				<a
+					class="App-link"
+					href="https://vitejs.dev/guide/features.html"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Vite Docs
+				</a>
+			</p>
+		</header>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import logo from './logo.svg';
+import './App.css';
+
+const count = ref(0);
+
+const incrementCount = () => {
+	count.value++;
+};
+</script>
+
+<style scoped>
+/*  */
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vite-deploy-demo/',
+  plugins: [vue()],
+  base: '/',
 })
